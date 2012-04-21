134c6338a1ca0dfa5b07d16bddc57ba474315b96
anchalk <anchalk@seas.upenn.edu>
2012-04-21 04:36:29 -0400

    final server

--- server.c
+++ server.c
@@ -1,180 +1,162 @@
+#include "mysocket.h"
+#include <time.h>
+#include<pthread.h>
 
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
-
-/** Given a host and a port number, connect as a client to that host/port
- Return a file descriptor number corresponding to the new socket
- The socket fd number (returned) can be used as follows:
- @param host
- @param portno
- @return socketfd
-
+	long *publicKeyServer;
+	long *privateKeyServer;
+	long *publicKeyClient;
+	long *productOfPrimes_C;
+	long *productOfPrimes_S ;
+/*
+/*
+@param connection_fd - socket fd of the connection
+Recieves data from the client
  */
+void * reciveData(void *connection_fd) {
 
-int connectTo(char* host, int portno) {
-	int sockfd;
-	struct sockaddr_in serv_addr;
-	struct hostent *server;
-
-	printf("hostname=%s\n", host);
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	server = gethostbyname(host);
-
-	if (server == NULL) {
-		printf(" No such host\n ", NULL);
-		exit(0);
+	int *file_descriptor = (int*) connection_fd;
+	int result;
+	int n;
+	int i = 0;
+	long decrypt1=1;
+	long decrypt2 = 0;
+	while (1) {
+	
+		result = read(*file_descriptor, &n,sizeof(int));
+		if(result <= 0){
+		break;
+		}
+		i = 0;
+		while(i < n){
+			
+			result = read(*file_descriptor, &decrypt1,sizeof(long));
+			if(result <= 0){
+		break;
+		}
+			printf("%ld\n", *productOfPrimes_S);
+			decrypt2 = decrypt(decrypt1,*privateKeyServer,*productOfPrimes_S);
+			putchar(decrypt2);
+			i++;
+		}
+		printf("\n");
 	}
-
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr,
-			server->h_length);
-	serv_addr.sin_port = htons(portno);
-	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))
-			< 0) {
-		printf("Error Connecting\n", NULL);
-	}
-	return sockfd;
+	close(*file_descriptor);
+	exit(0);
+	
 }
 
-/**
- * Form a server listener that waits for new connections
- One listener can answer many connections
- This returns a file descriptor number that can be used
- to listen for "new connections," and perhaps spawn
- threads corresponding to those connections.
- * @param portno
- * @return
+/*
+@param connection_fd - socket fd of the connection
+Send data from the client
  */
-int makeListener(int portno) {
-	int sockfd;
-	struct sockaddr_in serv_addr;
+void * sendData(void * connection_fd) {
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {
-		printf("ERROR opening socket\n", NULL);
-		exit(0);
+	long * file_descriptor = (long*) connection_fd;
+
+	int n;
+	int i = 0
+	int value = 0;
+	long encrypt1[1000];
+	long ch = 0;
+	int result = 0;
+	printf("\n\t\t\t-------------------------\n");
+	printf("\t\t\tWelcome to the chat room\n");
+	printf("\t\t\t-------------------------\n");
+	printf("To quit press q or Q\n");
+	while(1){
+		char buffer[1000] = {0};
+		
+		while((ch = getchar())!='\n'){
+	    		encrypt1[i] = encrypt(ch, *publicKeyClient,*productOfPrimes_C);
+			i++;
+			buffer[i] = ch;
+		}
+		if (strcasecmp(buffer,"\\q") == 0){
+			break;
+		}
+		result = write(*file_descriptor, &i, sizeof(i));
+		if (result <= 0) break;
+		n = 0;
+		while(n < i){
+			printf("encrypt1 = %ld\n",encrypt1[n]);
+			result = write(*file_descriptor, &encrypt1[n], sizeof(encrypt1[n]));
+			if (result <= 0){
+				break;
+			}
+			n++;
+		}
+		if (result <= 0) {
+			break;
+		}
+		i = 0;
+		
 	}
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-		printf("ERROR on binding\n", NULL);
-		exit(0);
-	}
+	close(*file_descriptor);
+}	
 
-	listen(sockfd, 15);
-
-	return sockfd;
+// sends public key to the client
+void *sendPublicKeyOfServer(long *publicKeyServer,long *productOfPrimes_S,void *connection_fd){
+	int * file_descriptor = (int*) connection_fd;
+	int length = sizeof(publicKeyServer);
+	int n;
+	n = write(*file_descriptor,publicKeyServer,length);
+	n = write(*file_descriptor,productOfPrimes_S,sizeof(productOfPrimes_S));
 }
 
-/**
- *
- *  Given an fd number provided by makeListener,
- block until a new connection is received.
- Return the file descriptor number for that
- new connection 
- @param sockfd
- @return
- */
-
-int listenFor(int sockfd) {
-	int newsockfd;
-	socklen_t clilen;
-	struct sockaddr_in cli_addr;
-
-	clilen = sizeof(cli_addr);
-
-	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-	if (newsockfd < 0) {
-		printf("ERROR on accept\n", NULL);
-	}
-	return newsockfd;
-}
-
-
-
-void * reciveData(void *connection_fd){
-
-int * file_descriptor = (int*)connection_fd;
-char buffer[1024];
-int n;
-bzero(buffer, 1024);
-while(1){
-	
-	n =  read(*file_descriptor, buffer, sizeof(buffer));
-	
-	printf(" %s\n", buffer);
-	 if ((strcmp(buffer , "q") == 0) || strcmp(buffer , "Q") == 0){
-		n=  write(*file_descriptor, buffer, strlen(buffer));
-		return;
-}
-	
-
-	
-
-}
-}
-
-void * sendData(void * connection_fd){
-
-int * file_descriptor = (int*)connection_fd;
-
-char send_data[1024];
-char buffer[1024];
-bzero(buffer, 1024);
-int n;
-
-while(1){
-
-	n =printf("Type Something (q or Q to quit):");
-	gets(send_data);
-	bzero(buffer, 1024);
-	 if ((strcmp(send_data , "q") == 0) || strcmp(send_data , "Q") == 0){
-     		
-		return;
-	}
-	else{
-
-	strncpy(buffer, send_data, strlen(send_data)+1);
-	}
-	 n =write(*file_descriptor, buffer, strlen(buffer));
-	
-}
+void *receivePublicKeyOfClient(long *publicKeyClient,long *productOfPrimes_C,void *connection_fd){
+	int * file_descriptor = (int*) connection_fd;
+	int length = sizeof(publicKeyClient);
+	int n;
+	n = read(*file_descriptor,publicKeyClient,length);
+	n = read(*file_descriptor,productOfPrimes_C,length);
+	//printf("Public key of server %ld",*publicKeyClient);
+	//printf("Public key of server %ld",*productOfPrimes_C);
+	printf("Public key of Client ( %ld , %ld)",*publicKeyClient ,*productOfPrimes_C);
 }
 
 int main(int argc, char** argv) {
+
+	unsigned int iseed = (unsigned int) time(NULL);
+	srand(iseed);
 	int srvfd, connection_fd;
-	char buffer[1024];
-	
+	char buffer[2048];
+	long primeNumber1 ,primeNumber2;
 	pthread_t recieveDataThread, sendDataThread;
-	
-	if(argc < 2) {
-	   printf("Usage: %s port\n", argv[0]);
-	   exit(0);
+
+
+	publicKeyServer = (long*)malloc(sizeof(long));
+	privateKeyServer = (long*)malloc(sizeof(long));
+	publicKeyClient = (long*)malloc(sizeof(long));
+	productOfPrimes_C = (long*)malloc(sizeof(long));
+	productOfPrimes_S = (long*)malloc(sizeof(long));
+
+
+	if (argc < 2) {
+		printf("Usage: %s port\n", argv[0]);
+		exit(0);
 	}
-	
+	//Generating prime numbers
+	primeNumber1 = generateRandomPrimeNum();
+	primeNumber2 = generateRandomPrimeNum();
+	//generating public and private keys
+	generateKeys(primeNumber1,primeNumber2,productOfPrimes_S,publicKeyServer,privateKeyServer);
+	printf("Public server key %ld\n",*publicKeyServer);
+	printf("Private server key %ld\n",*privateKeyServer);
+	printf("Public server pq %ld\n",*productOfPrimes_S);
+	//Making a connection
 	srvfd = makeListener(atoi(argv[1]));
 	connection_fd = listenFor(srvfd);
 
-        
+	//sending public key to the client
+	sendPublicKeyOfServer(publicKeyServer,productOfPrimes_S, &connection_fd);
+	receivePublicKeyOfClient(publicKeyClient,productOfPrimes_C, &connection_fd);
+	//creating threads for asynchronous communication
+	pthread_create(&recieveDataThread, NULL, reciveData, &connection_fd);
+	pthread_create(&sendDataThread, NULL, sendData, &connection_fd);
 
-	pthread_create(&recieveDataThread,NULL,reciveData,&connection_fd);
-	pthread_create(&sendDataThread,NULL,sendData,&connection_fd);
-	
-	pthread_join(recieveDataThread,NULL);
-	
-	
-	
-	close(connection_fd);	
+	pthread_join(recieveDataThread, NULL);
+
+	//closing all the connetions
+	close(connection_fd);
 	close(srvfd);
 }
